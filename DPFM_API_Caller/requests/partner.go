94b33a846d2e39bb97326b70a12ef9b14d5aaeba
Yurino Oga <yurino.o@latona.io>
2023-01-18 00:06:09 +0900

    release

---
+++ DPFM_API_Caller/requests/partner.go
@@ -0,0 +1,15 @@
+package requests
+
+type Partner struct {
+	DeliveryDocument        int     `json:"DeliveryDocument"`
+	PartnerFunction         string  `json:"PartnerFunction"`
+	BusinessPartner         int     `json:"BusinessPartner"`
+	BusinessPartnerFullName *string `json:"BusinessPartnerFullName"`
+	BusinessPartnerName     *string `json:"BusinessPartnerName"`
+	Organization            *string `json:"Organization"`
+	Country                 *string `json:"Country"`
+	Language                *string `json:"Language"`
+	Currency                *string `json:"Currency"`
+	ExternalDocumentID      *string `json:"ExternalDocumentID"`
+	AddressID               *int    `json:"AddressID"`
+}
